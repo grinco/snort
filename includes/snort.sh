b80e16e828662532e62e1d24a8f1896b20dad01f
oda-alexandre <oda.alexandre31@gmail.com>
2018-12-15 19:57:04 +0100

    # community rules

--- includes/snort.sh
+++ includes/snort.sh
@@ -2,7 +2,14 @@
 
 if [ -e /etc/snort/snort.conf ]
 then
-  service snort restart && snort -A console -c /etc/snort/snort.conf
+  service snort restart && \
+  snort -A console -c /etc/snort/snort.conf
 else
-  apt-get update && apt-get install -y snort && ldconfig && service snort restart && dpkg-reconfigure snort
+  apt-get update && \
+  apt-get install -y snort && \
+  ldconfig && \
+  sed -i "s|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g" /etc/snort/snort.conf && \
+  sed -i "s|include $RULE_PATH/community|#include $RULE_PATH/community|g" /etc/snort/snort.conf && \
+  service snort restart && \
+  dpkg-reconfigure snort
 fi
