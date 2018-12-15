e2ccc6a7aa12488512e6646cdffbc8fac8a55cc6
oda-alexandre <oda.alexandre31@gmail.com>
2018-12-15 20:20:31 +0100

    add sed external net

--- includes/snort.sh
+++ includes/snort.sh
@@ -9,6 +9,7 @@ else
   apt-get install -y snort && \
   ldconfig && \
   sed -i "s|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g" /etc/snort/snort.conf && \
+  sed -i "s|ipvar EXTERNAL_NET any|#ipvar EXTERNAL_NET any|g" /etc/snort/snort.conf && \
   sed -i "s|include $RULE_PATH/community|#include $RULE_PATH/community|g" /etc/snort/snort.conf && \
   service snort restart && \
   dpkg-reconfigure snort
