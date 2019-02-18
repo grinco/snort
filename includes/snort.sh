02c43671f5ce18f308e8326777d831e3443d4913
oda-alexandre <oda.alexandre31@gmail.com>
2019-02-18 16:32:22 +0100

    desactivation de pulledpork

--- includes/snort.sh
+++ includes/snort.sh
@@ -3,7 +3,7 @@
 # LANCEMENT DE SNORT SI LE FICHIER /etc/snort/snort.conf EXISTE
 if [ -e /etc/snort/snort.conf ]
 then
-  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
+#  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
   service snort restart && \
   snort -A console -c /etc/snort/snort.conf
 else
@@ -14,28 +14,28 @@ else
   ldconfig && \
 
 # INSTALLATION DE PULLEDPORK POUR LA GESTION AUTOMATISEE DES REGLES SNORT
-  git clone https://github.com/shirkdog/pulledpork.git pulledpork && \
-  cp pulledpork/pulledpork.pl /usr/local/bin && \
-  chmod +x /usr/local/bin/pulledpork.pl && \
-  cp pulledpork/etc/*.conf /etc/snort && \
-  rm -rf pulledpork && \
+#  git clone https://github.com/shirkdog/pulledpork.git && \
+#  cp pulledpork/pulledpork.pl /usr/local/bin && \
+#  chmod +x /usr/local/bin/pulledpork.pl && \
+#  cp pulledpork/etc/*.conf /etc/snort && \
+#  rm -rf pulledpork && \
 
 # CONFIGURATION DU FICHIER /etc/snort/snort.conf
   sed -i 's|ipvar EXTERNAL_NET any|#ipvar EXTERNAL_NET any|g' /etc/snort/snort.conf && \
   sed -i 's|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g' /etc/snort/snort.conf && \
 
 # CONFIGURATION DU FICHIER /etc/snort/pulledpork.conf
-  sed -i 's|rule_url=https://www.snort.org/reg-rules|#rule_url=https://www.snort.org/reg-rules|g' /etc/snort/pulledpork.conf && \
-  sed -i 's|/usr/local/etc/snort|/etc/snort|g' /etc/snort/pulledpork.conf && \
-  sed -i 's|sid_msg_version=1|sid_msg_version=2|g' /etc/snort/pulledpork.conf && \
-  sed -i 's|rule_url=https://rules.emergingthreatspro|#rule_url=https://rules.emergingthreatspro|g' /etc/snort/pulledpork.conf && \
-  sed -i 's|distro=FreeBSD-8-1|distro=Debian-9-0|g' /etc/snort/pulledpork.conf && \
-  mkdir /etc/snort/rules/iplists && \
-  touch /etc/snort/rules/iplists/default.blacklist && \
+#  sed -i 's|rule_url=https://www.snort.org/reg-rules|#rule_url=https://www.snort.org/reg-rules|g' /etc/snort/pulledpork.conf && \
+#  sed -i 's|/usr/local/etc/snort|/etc/snort|g' /etc/snort/pulledpork.conf && \
+#  sed -i 's|sid_msg_version=1|sid_msg_version=2|g' /etc/snort/pulledpork.conf && \
+#  sed -i 's|rule_url=https://rules.emergingthreatspro|#rule_url=https://rules.emergingthreatspro|g' /etc/snort/pulledpork.conf && \
+#  sed -i 's|distro=FreeBSD-8-1|distro=Debian-9-0|g' /etc/snort/pulledpork.conf && \
+#  mkdir /etc/snort/rules/iplists && \
+#  touch /etc/snort/rules/iplists/default.blacklist && \
 
 # CONFIGURATION DE PULLEDPORK
-  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
-  sed -i.old -e '572i include $RULE_PATH/snort.rules' /etc/snort/snort.conf && \
+#  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
+#  sed -i.old -e '572i include $RULE_PATH/snort.rules' /etc/snort/snort.conf && \
 
 # DEMARRAGE DE LA CONFIGURATION DE SNORT
   service snort restart && \
