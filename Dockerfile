71b9e576e31a40c7d6fdfa48fe089bf9865c57bf
oda-alexandre <oda.alexandre31@gmail.com>
2019-03-04 14:51:35 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -2,27 +2,35 @@ FROM debian:stretch-slim
 
 MAINTAINER https://oda-alexandre.com
 
+# VARIABLES
+ENV USER snort
 ENV DEBIAN_FRONTEND noninteractive
 
 # INSTALLATION DES PREREQUIS
 RUN apt-get update && apt-get install --no-install-recommends -y \
+sudo \
 curl \
 git \
 libcrypt-ssleay-perl \
-liblwp-useragent-determined-perl
+liblwp-useragent-determined-perl && \
+
+# AJOUT UTILISATEUR
+useradd -d /home/${USER} -m ${USER} && \
+passwd -d ${USER} && \
+adduser ${USER} sudo
+
+# SELECTION UTILISATEUR
+USER ${USER}
 
 # SELECTION ESPACE DE TRAVAIL
-WORKDIR /root/
+WORKDIR /home/${USER}
 
 # CREATION ESPACE DE TRAVAIL
 RUN mkdir snort
 
 # AJOUT INCLUDES
-COPY ./includes/snort.sh  /root/snort/snort.sh
-RUN chmod +x /root/snort/snort.sh
-
-# SELECTION ESPACE DE TRAVAIL
-WORKDIR /root/snort/
+COPY ./includes/snort.sh  /home/${USER}/snort.sh
+RUN sudo chmod +x /home/${USER}/snort.sh
 
 # COMMANDE AU DEMARRAGE DU CONTENEUR
 CMD ./snort.sh
--- includes/snort.sh
+++ includes/snort.sh
@@ -4,14 +4,14 @@
 if [ -e /etc/snort/snort.conf ]
 then
 #  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
-  service snort restart && \
-  snort -A console -c /etc/snort/snort.conf
+  sudo service snort restart && \
+  sudo snort -A console -c /etc/snort/snort.conf
 else
 
 # INSTALLATION DE SNORT SI LE FICHIER /etc/snort/snort.conf EXISTE PAS
-  apt-get update && \
-  apt-get install -y snort && \
-  ldconfig && \
+  sudo apt-get update && \
+  sudo apt-get install -y snort && \
+  sudo ldconfig && \
 
 # INSTALLATION DE PULLEDPORK POUR LA GESTION AUTOMATISEE DES REGLES SNORT
 #  git clone https://github.com/shirkdog/pulledpork.git && \
@@ -21,23 +21,23 @@ else
 #  rm -rf pulledpork && \
 
 # CONFIGURATION DU FICHIER /etc/snort/snort.conf
-  sed -i 's|ipvar EXTERNAL_NET any|#ipvar EXTERNAL_NET any|g' /etc/snort/snort.conf && \
-  sed -i 's|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g' /etc/snort/snort.conf && \
+  sed -i 's|ipvar EXTERNAL_NET any|#ipvar EXTERNAL_NET any|g' | sudo tee -a /etc/snort/snort.conf && \
+  sed -i 's|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g' | sudo tee -a /etc/snort/snort.conf && \
 
 # CONFIGURATION DU FICHIER /etc/snort/pulledpork.conf
-#  sed -i 's|rule_url=https://www.snort.org/reg-rules|#rule_url=https://www.snort.org/reg-rules|g' /etc/snort/pulledpork.conf && \
-#  sed -i 's|/usr/local/etc/snort|/etc/snort|g' /etc/snort/pulledpork.conf && \
-#  sed -i 's|sid_msg_version=1|sid_msg_version=2|g' /etc/snort/pulledpork.conf && \
-#  sed -i 's|rule_url=https://rules.emergingthreatspro|#rule_url=https://rules.emergingthreatspro|g' /etc/snort/pulledpork.conf && \
-#  sed -i 's|distro=FreeBSD-8-1|distro=Debian-9-0|g' /etc/snort/pulledpork.conf && \
-#  mkdir /etc/snort/rules/iplists && \
-#  touch /etc/snort/rules/iplists/default.blacklist && \
+#  sed -i 's|rule_url=https://www.snort.org/reg-rules|#rule_url=https://www.snort.org/reg-rules|g' | sudo tee -a /etc/snort/pulledpork.conf && \
+#  sed -i 's|/usr/local/etc/snort|/etc/snort|g' | sudo tee -a /etc/snort/pulledpork.conf && \
+#  sed -i 's|sid_msg_version=1|sid_msg_version=2|g' | sudo tee -a /etc/snort/pulledpork.conf && \
+#  sed -i 's|rule_url=https://rules.emergingthreatspro|#rule_url=https://rules.emergingthreatspro|g' | sudo tee -a /etc/snort/pulledpork.conf && \
+#  sed -i 's|distro=FreeBSD-8-1|distro=Debian-9-0|g' | sudo tee -a /etc/snort/pulledpork.conf && \
+#  sudo mkdir /etc/snort/rules/iplists && \
+#  sudo touch /etc/snort/rules/iplists/default.blacklist && \
 
 # CONFIGURATION DE PULLEDPORK
-#  /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
-#  sed -i.old -e '572i include $RULE_PATH/snort.rules' /etc/snort/snort.conf && \
+#  sudo /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
+#  sed -i.old -e '572i include $RULE_PATH/snort.rules' | sudo tee -a /etc/snort/snort.conf && \
 
 # DEMARRAGE DE LA CONFIGURATION DE SNORT
-  service snort restart && \
-  dpkg-reconfigure snort
+  sudo service snort restart && \
+  sudo dpkg-reconfigure snort
 fi
