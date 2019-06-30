c07faa40b122ed8abd2b972cb658be8b44f43fbd
oda-alexandre <oda.alexandre31@gmail.com>
2019-06-30 16:44:10 +0000

    step colorisation

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,35 @@
-# IMAGE TO USE
 FROM debian:stretch-slim
 
-# MAINTAINER
 MAINTAINER https://www.oda-alexandre.com/
 
-# VARIABLES
 ENV USER snort
 ENV DEBIAN_FRONTEND noninteractive
 
-# INSTALL PACKAGES
-RUN apt-get update && apt-get install --no-install-recommends -y \
+RUN echo -e '\033[36;1m ******* INSTALL PACKAGES ******** \033[0m' && \
+apt-get update && apt-get install --no-install-recommends -y \
 sudo \
 curl \
 git \
 libcrypt-ssleay-perl \
-liblwp-useragent-determined-perl && \
+liblwp-useragent-determined-perl
 
-# ADD USER
+RUN echo -e '\033[36;1m ******* ADD USER ******** \033[0m' && \
 useradd -d /home/${USER} -m ${USER} && \
 passwd -d ${USER} && \
 adduser ${USER} sudo
 
-# SELECT USER
+RUN echo -e '\033[36;1m ******* SELECT USER ******** \033[0m'
 USER ${USER}
 
-# SELECT WORKING SPACE
+RUN echo -e '\033[36;1m ******* SELECT WORKING SPACE ******** \033[0m'
 WORKDIR /home/${USER}
 
-# CREATION ESPACE OF TRAVAIL
-RUN mkdir snort
+RUN echo -e '\033[36;1m ******* CREATE WORKING SPACE ******** \033[0m' && \
+mkdir snort
 
-# ADD INCLUDES
+RUN echo -e '\033[36;1m ******* ADD APP ******** \033[0m'
 COPY ./includes/snort.sh  /home/${USER}/snort.sh
 RUN sudo chmod +x /home/${USER}/snort.sh
 
-# START THE CONTAINER
+RUN echo -e '\033[36;1m ******* CONTAINER START COMMAND ******** \033[0m'
 CMD ./snort.sh \
--- includes/snort.sh
+++ includes/snort.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# LANCEMENT OF SNORT SI THE FILE /etc/snort/snort.conf EXISTE
+echo -e '\033[36;1m ******* LAUNCH SNORT IF FILE /etc/snort/snort.conf EXISTS ******** \033[0m'
 if [ -e /etc/snort/snort.conf ]
 then
   # sudo /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
@@ -8,23 +8,24 @@ then
   sudo snort -A shell -c /etc/snort/snort.conf
 else
 
-# INSTALL OF SNORT SI THE FILE /etc/snort/snort.conf EXISTE PAS
+  echo -e '\033[36;1m ******* INSTALL SNORT IF FILE /etc/snort/snort.conf NOT EXISTS ******** \033[0m' && \
   sudo apt-get update && \
   sudo apt-get install -y snort && \
   sudo ldconfig && \
 
-# INSTALL OF PULLEDPORK FOR THE GESTION AUTOMATISEE OF THE REGLES SNORT
+  # echo -e '\033[36;1m ******* INSTALL OF PULLEDPORK FOR THE AUTOMATED MANAGEMENT OF THE SNOR RULES ******** \033[0m' && \
   # git clone https://github.com/shirkdog/pulledpork.git && \
   # sudo cp pulledpork/pulledpork.pl /usr/local/bin && \
   # sudo chmod +x /usr/local/bin/pulledpork.pl && \
   # sudo cp pulledpork/etc/*.conf /etc/snort && \
   # rm -rf pulledpork && \
 
-# CONFIG OF FILE /etc/snort/snort.conf
+  echo -e '\033[36;1m ******* CONFIG SNORT ******** \033[0m' && \
   sed -i 's|ipvar EXTERNAL_NET any|#ipvar EXTERNAL_NET any|g' | sudo tee -a /etc/snort/snort.conf && \
   sed -i 's|#ipvar EXTERNAL_NET !$HOME_NET|ipvar EXTERNAL_NET $HOME_NET|g' | sudo tee -a /etc/snort/snort.conf && \
 
-# CONFIG OF FILE /etc/snort/pulledpork.conf
+
+  # echo -e '\033[36;1m ******* CONFIG PULLEDPORK ******** \033[0m' && \
   # sed -i 's|rule_url=https://www.snort.org/reg-rules|#rule_url=https://www.snort.org/reg-rules|g' | sudo tee -a /etc/snort/pulledpork.conf && \
   # sed -i 's|/usr/local/etc/snort|/etc/snort|g' | sudo tee -a /etc/snort/pulledpork.conf && \
   # sed -i 's|sid_msg_version=1|sid_msg_version=2|g' | sudo tee -a /etc/snort/pulledpork.conf && \
@@ -32,12 +33,10 @@ else
   # sed -i 's|distro=FreeBSD-8-1|distro=Debian-9-0|g' | sudo tee -a /etc/snort/pulledpork.conf && \
   # sudo mkdir /etc/snort/rules/iplists && \
   # sudo touch /etc/snort/rules/iplists/default.blacklist && \
-
-# CONFIG OF PULLEDPORK
   # sudo /usr/local/bin/pulledpork.pl -c /etc/snort/pulledpork.conf -l && \
   # sed -i.old -e '572i include $RULE_PATH/snort.rules' | sudo tee -a /etc/snort/snort.conf && \
 
-# STARTING THE CONFIG OF SNORT
+  echo -e '\033[36;1m ******* START MANUAL CONFIG OF SNORT ******** \033[0m' && \
   sudo service snort restart && \
   sudo dpkg-reconfigure snort
 fi
